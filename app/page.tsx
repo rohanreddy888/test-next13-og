76c7f4cf2dcf4fcf20103d16373c09b8cf6e5cab
Rohan Reddy <rohanreddy888@gmail.com>
2023-03-07 21:18:49 +0530

    Initial Push

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,47 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "./page.module.css";
+import type { Metadata } from "next";
+const inter = Inter({ subsets: ["latin"] });
 
-const inter = Inter({ subsets: ['latin'] })
-
+export const metadata: Metadata = {
+  title: "Web3 Hack Statistics, Hackerboard - QuillAudits",
+  description:
+    "Get statistics and deep analysis of recent crypto hacks, vulnerabilities, and attack vectors around the web3 world.",
+  openGraph: {
+    title: "Web3 Hack Statistics, Hackerboard - QuillAudits",
+    url: "https://www.quillaudits.com/tools/hackerboard",
+    description:
+      "Get statistics and deep analysis of recent crypto hacks, vulnerabilities, and attack vectors around the web3 world.",
+    image: "https://quillaudits.com/api/og?amount=6B&number=1000",
+    images: [
+      {
+        url: "https://quillaudits.com/api/og?amount=6B&number=1000",
+        secureUrl: "https://quillaudits.com/api/og?amount=6B&number=1000",
+        alt: "Web3 Hack Statistics, Hackerboard - QuillAudits",
+        width: 1200,
+        height: 630,
+        type: "image/png",
+      },
+    ],
+    locale: "en-US",
+    type: "website",
+  },
+  alternates: {
+    canonical: "https://www.quillaudits.com/tools/hackerboard",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Web3 Hack Statistics, Hackerboard - QuillAudits",
+    description:
+      "Get statistics and deep analysis of recent crypto hacks, vulnerabilities, and attack vectors around the web3 world.",
+    creator: "@QuillAudits",
+    images: ["https://quillaudits.com/api/og?amount=6B&number=1000"],
+  },
+  robots: {
+    index: true,
+  },
+};
 export default function Home() {
   return (
     <main className={styles.main}>
@@ -18,7 +56,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -87,5 +125,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
